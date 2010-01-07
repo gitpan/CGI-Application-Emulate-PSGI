c3a3efd443cfaaef1c5ca6ad179cc645288f1682
Kazuho Oku <kazuhooku@gmail.com>
2010-01-07 06:31:52 +0000

    Import of KAZUHO/CGI-Application-Emulate-PSGI-0.02 from CPAN.
    gitpan-cpan-distribution: CGI-Application-Emulate-PSGI
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         KAZUHO/CGI-Application-Emulate-PSGI-0.02.tar.gz
    gitpan-cpan-author:       KAZUHO
    gitpan-cpan-maturity:     released

--- lib/CGI/Application/Emulate/PSGI.pm
+++ lib/CGI/Application/Emulate/PSGI.pm
@@ -4,50 +4,28 @@ use 5.008;
 use strict;
 use warnings;
 use CGI;
+use CGI::Emulate::PSGI;
+use CGI::Parse::PSGI;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub handler {
     my ($class, $code,) = @_;
     
     return sub {
         my $env = shift;
-        my $environment = {
-            GATEWAY_INTERFACE => 'CGI/1.1',
-            # not in RFC 3875
-            HTTPS => ( ( $env->{'psgi.url_scheme'} eq 'https' ) ? 'ON' : 'OFF' ),
-            SERVER_SOFTWARE => "CGI-Emulate-PSGI",
-            REMOTE_ADDR     => '127.0.0.1',
-            REMOTE_HOST     => 'localhost',
-            REMOTE_PORT     => int( rand(64000) + 1000 ),    # not in RFC 3875
-            # REQUEST_URI     => $uri->path_query,             # not in RFC 3875
-            ( map { $_ => $env->{$_} } grep !/^psgi\./, keys %$env ),
-            CGI_APP_RETURN_ONLY => 1,
-        };
         my $output = do {
+            local %ENV = (
+                %ENV,
+                CGI::Emulate::PSGI->emulate_environment($env),
+                CGI_APP_RETURN_ONLY => 1,
+            );
             local *STDIN  = $env->{'psgi.input'};
             local *STDERR = $env->{'psgi.errors'};
-            local @ENV{keys %$environment} = values %$environment;
             CGI::initialize_globals();
             $code->();
         };
-        my $status = 200;
-        my ($headers, $body) = split /\r?\n\r?\n/, $output, 2;
-        my @headers = map { split /:\s*/, $_, 2 } split /\r?\n/, $headers;
-        for (my $i = 0; $i < @headers;) {
-            if ($headers[$i] =~ /^status$/i) {
-                $status = $headers[$i + 1];
-                $status =~ s/\s+.*$//; # only keep the digits
-                splice @headers, $i, 2;
-            } else {
-                $i += 2;
-            }
-        }
-        return [
-            $status,
-            \@headers,
-            [ $body ],
-        ];
+        return CGI::Parse::PSGI::parse_cgi_output(\$output);
     };
 }
 
@@ -60,30 +38,39 @@ CGI::Application::Emulate::PSGI - a legacy-code-friendly PSGI adapter for CGI::A
 
 =head1 SYNOPSIS
 
+Create a PSGI application from a L<CGI::Application> project:
+
     # if using CGI::Application
-    my $app = CGI::Application::Emulate::PSGI->handler(sub {
+    my $psgi_app = CGI::Application::Emulate::PSGI->handler(sub {
         my $webapp = WebApp->new();
         $webapp->run();
     });
 
     # if using CGI::Application::Dispatch
-    my $app = CGI::Application::Emulate::PSGI->handler(sub {
+    my $psgi_app = CGI::Application::Emulate::PSGI->handler(sub {
         WebApp::Dispatch->dispatch();
     });
 
+See L<plackup> for options for running a PSGI application.
+
 =head1 DESCRIPTION
 
-CGI::Application::Emulate::PSGI is a runner to run L<CGI::Application> as a PSGI application.  Differences from L<CGI::Application::PSGI> are:
+CGI::Application::Emulate::PSGI allows a project based on L<CGI::Application> to run as a PSGI application.  Differences from L<CGI::Application::PSGI> are:
 
 =over 4
 
-=item uses L<CGI.pm> instead of L<CGI::PSGI>
+=item uses L<CGI.pm> directly instead of L<CGI::PSGI>
 
-L<CGI::Application::PSGI> (that uses L<CGI::PSGI>) does not support programs calling L<CGI.pm> in func-style (like CGI::virtual_host()).  CGI::Application::Emulate::PSGI sets up environment variables so that codes using L<CGI.pm> will work.
+L<CGI::Application::PSGI> (that uses L<CGI::PSGI>) does not support programs calling L<CGI.pm> in func-style (like CGI::virtual_host()).  CGI::Application::Emulate::PSGI sets up environment variables so that code using L<CGI.pm> will work. Both approaches explictly use CGI.pm as the query object.
 
 =item compatible with L<CGI::Application::Dispatch>
 
 The interface of CGI::Application::Emulate::PSGI is different from L<CGI::Application::PSGI>, and is compatible with L<CGI::Application::Dispatch>.
+
+=item headers are parsed and re-generated.
+
+This difference is in favor of L<CGI::Application::PSGI>, which more directly generates the HTTP headers in PSGI format. This module
+requires additional processing: First CGI::Application builds the full response including the headers and body, then we parse the final result back into the header and body format called for by the PSGI spec.
 
 =back
 
@@ -98,6 +85,7 @@ it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-L<CGI::Application::PSGI>
+L<CGI::Application::PSGI>, L<CGI>
+
 
 =cut
